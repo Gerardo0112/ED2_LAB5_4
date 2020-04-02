f4ac771374c6097027dd4479cb6c4341af716958
unknown <gerar0112@gmail.com>
2020-04-01 19:29:35 -0600

    Lectura para cifrado de zig zag.

--- ED2_LAB5_4/Codes/ZigZag_Encryption.cs
+++ ED2_LAB5_4/Codes/ZigZag_Encryption.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Web;
 
 namespace ED2_LAB5_4.Codes
 {
     public class ZigZag_Encryption
     {
+        //Lectura para el cifrado.
+        public List<byte> encryption(string file, int length)
+        {
+            //Lista para bytes.
+            var list = new List<byte>();
+            using(var stream = new FileStream(file, FileMode.Open))
+            {
+                //Lectura.
+                using(var reading = new BinaryReader(stream))
+                {
+                    //Almacenamiento.
+                    var bytes = new byte[length];
+                    while(reading.BaseStream.Position != reading.BaseStream.Length)
+                    {
+                        //Lee los bytes.
+                        bytes = reading.ReadBytes(length);
+                        foreach(byte bit in bytes)
+                        {
+                            //Agregando a la lista.
+                            list.Add(bit);
+                        }
+                    }
+                }
+            }
+            return list;
+        }
     }
 }
